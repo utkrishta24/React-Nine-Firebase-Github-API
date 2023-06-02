6a52b88ce2ae08daa3d8893ba9e3c8301334b3fb
Utkrishta Sinha <krishsinha2406@gmail.com>
2023-06-03 00:32:10 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.min.css"
+
+import firebase from "firebase/compat/app"
+import "firebase/compat/auth";
+
+import Home from "./components/Home"
+import Signin from "./components/SignIn"
+import Signup from "./components/SignUp"
+import PageNotFound from "./components/PageNotFound"
+import { UserContext } from './context/UserContext';
+import Footer from './layout/Footer';
+import Header from './layout/Header';
+import firebaseConfig from "./config/firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
+
+const App = () => {
+  const[user, setUser] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ToastContainer/>
+      <UserContext.Provider value={{user,setUser}}>
+      <Header/>
+      <Routes>
+        <Route exact path='/' Component={Home} />
+        <Route exact path='/signin' Component={Signin} />
+        <Route exact path='/signup' Component={Signup} />
+        <Route exact path='*' Component={PageNotFound} />
+      </Routes>
+      <Footer/>
+      </UserContext.Provider>
+    </Router>
   );
 }
 
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCQKSYz3JZaOT7zBbfF07oncypIbL2-I8g",
+    authDomain: "react-git-app-8d636.firebaseapp.com",
+    projectId: "react-git-app-8d636",
+    storageBucket: "react-git-app-8d636.appspot.com",
+    messagingSenderId: "164392242585",
+    appId: "1:164392242585:web:45d875761fe689a21b1708",
+    measurementId: "G-GDEY80K5VJ"
+  };
